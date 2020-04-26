b596fb844b511a63b723598e2cd7a91dbc98f5a2
Tumwesigye <Tumwesigye>
2020-04-26 18:14:59 +0300

    My first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos'
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  },
+  //To do list
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "To do 1",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "To do 2",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "To do 3",
+          completed: false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id )
+    }
+  }
+}
+</script>
+
+<style>
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="todo-item" v-bind:class="{'is-complete': todo.completed}">
+        <!-- :class="[todo.name === 'Jerome' ? '!is-complete':'is-complete']" -->
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{todo.title}}
+            <button @click="$emit('del-todo', todo.id)">X</button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ['todo'],
+    methods: {
+        markComplete() {
+           return this.todo.completed = !this.todo.completed
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted
+}
+.is-complete {
+    color: #ff0000;
+  text-decoration: line-through;
+}
+.del {
+  background: #ff0000;
+}
+
+</style> 
